17497643c618bbcdf2d35fe43383f0349a28fc18
The cool Treecko <77108834+The-cool-Treecko@users.noreply.github.com>
2021-11-25 16:06:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function preload() {}
+var nose_x;
+var nose_y; 
+
+function preload() {
+    noseImg = loadImage("https://i.postimg.cc/vH3qYRwG/THE-MOUSTACHE-EQUATION-04.png")
+}
 
 function setup() {
     canvas = createCanvas(300,300);
@@ -18,13 +23,16 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = "+results[0].pose.nose.x);
-        console.log("nose y = "+results[0].pose.nose.y);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("nose x = "+nose_x);
+        console.log("nose y = "+nose_y);
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(noseImg,nose_x-25, nose_y+5,50,30);
 }
 
 function saveImg() {
